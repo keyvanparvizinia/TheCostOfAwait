6f232f0e0daebd20a8c304f21f1571a9fa0cbf66
K1 <parvizi.k1@hotmail.com>
2023-06-12 11:15:39 +0330

    Add project files.

---
+++ TheCostOfAwait/AwaitVsElide.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace TheCostOfAwait;
+
+[MemoryDiagnoser]
+public class AwaitVsElide
+{
+    private static readonly Task<string> ExampleTask = Task.FromResult("Hello World!");
+
+    [Benchmark]
+    public async Task<string> GetStringWithoutAsync() => await GetStringWithoutAsyncCore();
+
+    [Benchmark]
+    public async Task<string> GetStringWithAsync() => await GetStringWithAsyncCore();
+
+    private static Task<string> GetStringWithoutAsyncCore() => ExampleTask;
+    private static async Task<string> GetStringWithAsyncCore() => await ExampleTask;
+
+}
---
+++ TheCostOfAwait/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using TheCostOfAwait;
+
+BenchmarkRunner.Run<AwaitVsElide>();
